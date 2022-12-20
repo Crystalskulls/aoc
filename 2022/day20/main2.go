8f0bae075804c400e9394cb5c8b06701a61f8b02
Benedikt Müller <bene93.mueller@web.de>
2022-12-20 23:25:33 +0100

    aoc 2022 - day20

---
+++ 2022/day20/main2.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Number struct {
+	value      int
+	realNumber int
+	prevNumber *Number
+	nextNumber *Number
+}
+
+func main() {
+	numbers, zeroNumber := readInput()
+	for i := 0; i < 10; i++ {
+		mixing(numbers)
+	}
+	fmt.Printf("PartOne: %d\n", sumOfGroveCoordinates(numbers, zeroNumber))
+}
+
+func sumOfGroveCoordinates(numbers []*Number, zeroNumber *Number) (sum int) {
+	for i := 1; i <= 3; i++ {
+		number := zeroNumber
+		j := (i * 1000) % len(numbers)
+		for k := 1; k <= j; k++ {
+			number = number.nextNumber
+		}
+		fmt.Println(number.value)
+		sum += number.realNumber
+	}
+	return sum
+}
+
+func mixing(numbers []*Number) {
+	for _, currentNumber := range numbers {
+		if currentNumber.value > 0 {
+			currentNumber.moveRight()
+		} else {
+			currentNumber.moveLeft()
+		}
+	}
+}
+
+func (number *Number) moveRight() {
+	for i := 0; i < number.value; i++ {
+		nextNumber := number.nextNumber
+		currentNumberPrev := number.prevNumber
+		number.prevNumber = nextNumber
+		number.nextNumber = nextNumber.nextNumber
+		nextNumber.nextNumber = number
+		nextNumber.prevNumber = currentNumberPrev
+		nextNumber.prevNumber.nextNumber = nextNumber
+		number.nextNumber.prevNumber = number
+	}
+}
+
+func (number *Number) moveLeft() {
+	for i := 0; i > number.value; i-- {
+		prevNumber := number.prevNumber
+		prevNumberPrev := prevNumber.prevNumber
+		prevNumber.nextNumber = number.nextNumber
+		prevNumber.prevNumber = number
+		number.nextNumber = prevNumber
+		number.prevNumber = prevNumberPrev
+		prevNumber.nextNumber.prevNumber = prevNumber
+		number.prevNumber.nextNumber = number
+	}
+}
+
+func readInput() (numbers []*Number, zeroNumber *Number) {
+	input, _ := os.ReadFile("input.txt")
+	lines := strings.Split(string(input), "\n")
+	numbers = make([]*Number, len(lines))
+	for i, line := range lines {
+		n, _ := strconv.Atoi(line)
+		numbers[i] = &Number{
+			value:      n * 811589153 % (len(numbers) - 1),
+			realNumber: n * 811589153,
+		}
+		if n == 0 {
+			zeroNumber = numbers[i]
+		}
+	}
+	for j := range numbers {
+		if j == 0 {
+			numbers[j].prevNumber = numbers[len(numbers)-1]
+			numbers[j].nextNumber = numbers[j+1]
+		} else if j == len(numbers)-1 {
+			numbers[j].nextNumber = numbers[0]
+			numbers[j].prevNumber = numbers[j-1]
+		} else {
+			numbers[j].nextNumber = numbers[j+1]
+			numbers[j].prevNumber = numbers[j-1]
+		}
+	}
+	return numbers, zeroNumber
+}
+
+func (number *Number) String() string {
+	return fmt.Sprintf("%p: %d, next: %p, prev: %p\n", number, number.value, number.nextNumber, number.prevNumber)
+}
